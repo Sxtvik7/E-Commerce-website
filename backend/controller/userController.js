29f75c8cbe193f8c251e6049143bbe0519f27587
Satvik <Satvikgovale7@gmail.com>
2024-01-09 15:36:42 +0530

    adding some controller function and generate token UtilFunction

--- backend/controller/userController.js
+++ backend/controller/userController.js
@@ -1,6 +1,6 @@
 import asyncHandler from "../middleware/asyncHandler.js";
 import User from "../models/userModel.js";
-import jwt from "jsonwebtoken";
+import generateToken from "../utils/generateToken.js";
 
 //Logging in User
 const authUser = asyncHandler(async (req, res) => {
@@ -9,53 +9,108 @@ const authUser = asyncHandler(async (req, res) => {
   const user = await User.findOne({ email });
 
   if (user && (await user.matchPassword(password))) {
+    generateToken(res, user._id);
 
-    const token = jwt.sign({ userId: user._id}, process.env.JWT_SECRET, {expiresIn: '30d'});
-
-    //set jwt as HTTP-only cookie
-    res.cookie('jwt', token, {
-      httpOnly: true,
-      secure: process.env.NODE_ENV !== 'development',
-      sameSite: 'strict',
-      maxAge: 30 * 24 * 60 * 1000 //30 days
-    });
-
-    res.json({
+    res.status(200).json({
       _id: user._id,
       name: user.name,
       email: user.email,
       isAdmin: user.isAdmin,
     });
-  }else{
+  } else {
     res.status(401);
     throw new Error("Invalid Email or Password");
   }
-
 });
+//--------------------------------------------------------------------------------------
 
 //Signup User | Register New User
 const registerUser = asyncHandler(async (req, res) => {
-  res.send("user Registered");
+  const { name, email, password } = req.body;
+
+  const userExist = await User.findOne({ email });
+
+  if (userExist) {
+    res.status(400);
+    throw new Error("User already exist, please login");
+  }
+
+  const user = await User.create({ name, email, password });
+
+  if (user) {
+    generateToken(res, user._id);
+
+    res.status(201).json({
+      _id: user._id,
+      name: user.name,
+      email: user.email,
+      isAdmin: user.isAdmin,
+    });
+  } else {
+    res.status(500);
+    throw new Error("Invalid user data");
+  }
 });
+//--------------------------------------------------------------------------------------
 
 //Logout User
 const logoutUser = asyncHandler(async (req, res) => {
-  res.cookie('jwt','', {
+  res.cookie("jwt", "", {
     httpOnly: true,
-    expires: new Date(0)
+    expires: new Date(0),
   });
-  res.status(200).json({message: 'Logged out successfully'});
+  res.status(200).json({ message: "Logged out successfully" });
 });
+
+//--------------------------------------------------------------------------------------
 
 //Get User's Profile
 const getUserProfile = asyncHandler(async (req, res) => {
-  res.send("User Profile");
+  const user = await User.findById(req.user._id);
+
+  if (user) {
+    res.status(200).json({
+      _id: user._id,
+      name: user.name,
+      email: user.email,
+      isAdmin: user.isAdmin,
+    });
+  } else {
+    res.status(404);
+    throw new Error("User not found");
+  }
 });
+
+//--------------------------------------------------------------------------------------
 
 //Update User Profile
 const updateUserProfile = asyncHandler(async (req, res) => {
-  res.send(" update User Profile");
+  const user = await User.findById(req.user._id);
+
+  if (user) {
+    user.name = req.body.name || user.name;
+    user.email = req.body.email || user.email;
+
+    if (req.body.password) {
+      user.password = req.body.password;
+    }
+
+    const updatedUser = await user.save();
+
+    res.status(200).json({
+      _id: updatedUser._id,
+      name: updatedUser.name,
+      email: updatedUser.email,
+      isAdmin: updatedUser.isAdmin,
+    });
+
+  } else {
+    res.status(404);
+    throw new Error("User not found");
+  }
 });
+
+//--------------------------------------------------------------------------------------
 
 //delete User Profile
 const deleteUser = asyncHandler(async (req, res) => {
--- backend/models/userModel.js
+++ backend/models/userModel.js
@@ -32,6 +32,16 @@ userSchema.methods.matchPassword = async function (enteredPassword) {
   return await bcrypt.compare(enteredPassword, this.password);
 };
 
+//hashing the password before saving it to the database.
+userSchema.pre('save', async function (next){
+  if(!this.isModified('password')){
+    next();
+  }
+
+  const salt = await bcrypt.genSalt(8);
+  this.password = await bcrypt.hash(this.password, salt);
+});
+
 const User = mongoose.model("User", userSchema);
 
 export default User;
